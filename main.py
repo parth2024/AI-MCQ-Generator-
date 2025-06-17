45c8b5e2a7c9ed310f00da998088a63769788d51
parth2024 <156824891+parth2024@users.noreply.github.com>
2025-06-17 21:58:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
+import os
 
 app = Flask(__name__)
 CORS(app) 
@@ -81,6 +82,6 @@ Rules:
 
 
 if __name__ == '__main__':
-   import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
